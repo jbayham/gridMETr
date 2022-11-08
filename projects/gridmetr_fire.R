aebf8030424c182e946e8e1fdd1e90c0f9a0cbcf
Jude Bayham <jbayham@gmail.com>
2022-11-08 15:57:15 -0700

    update extract for farm worker and pods projects

--- projects/gridmetr_fire.R
+++ projects/gridmetr_fire.R
@@ -1,36 +1,27 @@
-#This implementation of gridmetr compiles weather data for the project
-#investigating the impact of pollution on farm worker productivity
-
-#Project setup
-source("project_init.R")
-
-library(pacman)
-p_load(tidyverse,lubridate,data.table,sf,ncdf4,janitor,raster,exactextractr,furrr,conflicted,measurements)
-
-conflict_prefer("select", "dplyr")
-conflict_prefer("year", "lubridate")
-######################
-#Download gridmet data
+#This script downloads the relevant gridmet data and extracts the data associated with the fire points
 
 #Define variables to use to construct data (see _ref/variables_reference.csv)
-g.vars <- c("pr","rmin","rmax","tmmn","tmmx","vs","erc","bi")
+g.vars <- c("pr","rmin","rmax","tmmn","tmmx","vs","bi","erc")
 
 #Define years to construct data
-g.years <- seq.int(2018,2021)
+g.years <- c(2020,2021)   #seq.int(2008,2020)
 #########################################################################
 #Get netcdf data from source and cache copies in data folder 
 gridmetr_download(variables = g.vars,
                   years = g.years)  
 
-
 ##############################################################
 #load farms
-farms <- st_read("cache/farms.geojson") %>%
-  st_transform(4326)
+target_points <- read_csv("cache/fires_2021.csv") %>%
+  janitor::clean_names() %>%
+  select(inc_name,inc_num,latitude,longitude,disc_date) %>%
+  mutate(disc_date = mdy(disc_date)) %>%
+  st_as_sf(coords=c("longitude","latitude"),crs=4326)
+
+#mapview::mapview(fires)
 
 #Open the connection to the netCDF file
-nc <- nc_open("data/tmmx/tmmx_2010.nc")
-
+nc <- nc_open("inputs/data/tmmx/tmmx_2010.nc")
 
 #Extract lat and lon vectors
 nc_lat <- ncvar_get(nc = nc, varid = "lat")
@@ -44,22 +35,17 @@ nc.coords <- expand.grid(lon=nc_lon,lat=nc_lat) %>%
 readin.proj=4326 #because it works with the lat and lons provided
 
 #Alt method reading as raster then using exact_extract
-tmmx <- raster("data/tmmx/tmmx_2010.nc")
-crs(tmmx)<-CRS("+init=epsg:4326")
-#mapview::mapview(tmmx)
+sample_raster <- raster("inputs/data/tmmx/tmmx_2021.nc")
+crs(sample_raster)<-CRS("+init=epsg:4326")
+#mapview::mapview(sample_raster)
 
-cell_index <- extract(tmmx,farms,cellnumbers=T,df=T)
+cell_index <- extract(sample_raster,target_points,cellnumbers=T,df=T) %>%
+  mutate(name=target_points$inc_num,
+         disc_date=target_points$disc_date)
 
-#check coverage of tribal areas - confirmed that all conus geometries are included
-# check_set <- aiannh$geoid[!(unique(aiannh$geoid) %in% unique(cell_index$geoid))]
-# aiannh %>%
-#   filter(geoid %in% check_set) %>%
-#   mapview::mapview()
-
-
+#Verify merge occurred on cells
 bridge <- inner_join(cell_index,nc.coords) %>%
-  select(cells,lon,lat) 
-bridge$name <- farms$name
+  select(cells,lon,lat,name,disc_date) 
 
 ###################################################
 
@@ -69,7 +55,7 @@ file.list <- expand.grid(g.vars,g.years,stringsAsFactors = F) %>%
          file.name = str_c(var,"_",year,".nc")) %>% 
   arrange(var) 
 
-allheat <- paste0("data/",file.list$var,"/",file.list$file.name)
+allheat <- paste0("inputs/data/",file.list$var,"/",file.list$file.name)
 heat1 <- allheat[1]
 
 plan(multisession(workers = 15))
@@ -107,9 +93,9 @@ future_map(allheat,
                     variable.name = "date",
                     value.name = "value") 
              
-             dir_name = str_c("cache/",str_split(heat1,"/")[[1]][[2]])
+             dir_name = str_c("cache/",str_split(heat1,"/")[[1]][[3]])
              if(!dir.exists(dir_name)) dir.create(dir_name)
-             write_rds(var.block,paste0(dir_name,"/",str_split(heat1,"/")[[1]][[2]],"_",str_sub(heat1,-7,-4),".rds"))
+             write_rds(var.block,paste0(dir_name,"/",str_split(heat1,"/")[[1]][[3]],"_",str_sub(heat1,-7,-4),".rds"))
              
            },.progress = T)
 
@@ -117,32 +103,45 @@ future_map(allheat,
 ############################################################
 #Read in all individual years
 
-farm_weather <- dir("cache",pattern = ".rds",recursive = T,full.names = F) %>%
+fire_weather <- dir("cache",pattern = ".rds",recursive = T,full.names = F) %>%
   map_dfr(function(x){
     out <- read_rds(str_c("cache/",x)) %>%
       mutate(var=dirname(x))
-  })
+  }) %>%
+  mutate(date=as_date(date)) %>% 
+  inner_join(st_set_geometry(target_points,NULL) %>% select(inc_num,disc_date),
+             by=c("name"="inc_num")) %>% 
+  filter(date %within% (as_date(disc_date-14) %--% disc_date))
 
-farm_weather %>% 
-  filter(year(date)==2012) %>%
-  mutate(date=as_date(date)) %>%
-  mutate(value=ifelse(str_detect(var,"vs"),   #Windspeed m/s to mph
-                      measurements::conv_unit(value,"m_per_sec","mph"),
-                      value),
-         value=ifelse(str_detect(var,"pr"),   #precip mm to inch
-                      measurements::conv_unit(value,"mm","inch"),
-                      value),
-         value=ifelse(str_detect(var,"tmm"),             #Temp Kelvin to F
-                      measurements::conv_unit(value,"K","F"),
-                      value)
-  ) %>%
-  ggplot(aes(x=date,y=value,color=name)) +
+
+
+
+fire_weather %>%
+  filter(name==sample(fire_weather$name,1)) %>%
+  ggplot(aes(x=date,y=value)) +
   geom_point() +
-  scale_x_date(date_breaks = "3 months",date_labels = "%b") +
   facet_wrap(~var,scales = "free")
 
-farm_weather %>%
-  mutate(date=as_date(date)) %>%
+# fire_weather %>% 
+#   filter(year(date)==2012) %>%
+#   mutate(date=as_date(date)) %>%
+#   mutate(value=ifelse(str_detect(var,"vs"),   #Windspeed m/s to mph
+#                       measurements::conv_unit(value,"m_per_sec","mph"),
+#                       value),
+#          value=ifelse(str_detect(var,"pr"),   #precip mm to inch
+#                       measurements::conv_unit(value,"mm","inch"),
+#                       value),
+#          value=ifelse(str_detect(var,"tmm"),             #Temp Kelvin to F
+#                       measurements::conv_unit(value,"K","F"),
+#                       value)
+#   ) %>%
+#   ggplot(aes(x=date,y=value,color=name)) +
+#   geom_point() +
+#   scale_x_date(date_breaks = "3 months",date_labels = "%b") +
+#   facet_wrap(~var,scales = "free")
+
+fire_weather %>%
+  select(-disc_date) %>%
   mutate(value=ifelse(str_detect(var,"vs"),   #Windspeed m/s to mph
                       measurements::conv_unit(value,"m_per_sec","mph"),
                       value),
@@ -153,7 +152,6 @@ farm_weather %>%
                       measurements::conv_unit(value,"K","F"),
                       value)
   ) %>%
-  pivot_wider(names_from = var,values_from = value) %>%
-  write_csv("cache/farm_weather.csv")
+  pivot_wider(id_cols=c(name,date),names_from = var,values_from = value,values_fn = mean) %>%
+  write_csv("cache/fire_weather.csv")
 
-mapview::mapview(farms)
